5c5211ae45034be9b2014541130151639900f32b
FernandezLJuan <juanlopezfernandes@gmail.com>
2021-09-11 21:42:27 +0200

    suscriptor y contador con ROS y python

---
+++ suscriber_py/src/suscriber.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+
+def callback(msg):
+
+	print msg.data
+
+rospy.init_node('suscriptor_basico')
+sub=rospy.Subscriber('/contador',Int32,callback)
+rospy.spin()
