d1a726cefc9764db001ced5f2acb1d86ce0932ad
FernandezLJuan <juanlopezfernandes@gmail.com>
2021-09-23 16:37:58 +0200

    publicador mensajes personalizados

--- publicador/src/pub.py
+++ publicador/src/pub.py
@@ -4,15 +4,15 @@ import rospy
 from publicador.msg import datos
 
 rospy.init_node('publicador')
-pub=rospy.Publisher('/mensajes_ejemplo',datos,queue_size=1)
+pub=rospy.Publisher('/mensajePers',datos,queue_size=1)
 rate=rospy.Rate(2)
 
 mensaje=datos()
 mensaje.edad=18
-mensaje.altura=1.75
+mensaje.coeficiente=1.70
 mensaje.nombre="Juan"
 
 while not rospy.is_shutdown():
-	
-	pub.publish(datos)
+
+	pub.publish(mensaje)
 	rate.sleep()
